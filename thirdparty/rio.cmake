ef20a908893f812dd8e852368b3a4ac13aa9dbb9
stefan.poeter@cloud-automation.de <stefan.poeter@cloud-automation.de>
2017-10-04 14:19:48 +0200

    Build RikerIO Server as external project.

--- thirdparty/rio.cmake
+++ thirdparty/rio.cmake
@@ -1,14 +1,14 @@
-set(RIO_PREFIX      rio101)
-set(RIO_VERSION     v1.0.1-dev)
+set(RIO_PREFIX      rio_2_0)
+set(RIO_VERSION     v2.0.latest)
 set(RIO_BIN_DIR     ${PROJECT_BINARY_DIR}/${RIO_PREFIX}-build)
 set(RIO_INSTALL_DIR ${PROJECT_BINARY_DIR}/${RIO_PREFIX}-install)
 
 ExternalProject_Add(${RIO_PREFIX}
     PREFIX ${RIO_PREFIX}
-    GIT_REPOSITORY git@github.com:stefanpoeter/rikerio-server.git
+    GIT_REPOSITORY git@github.com:RikerIO/rikerio-server.git
     GIT_TAG ${RIO_VERSION}
     BINARY_DIR ${RIO_BIN_DIR}
-    CMAKE_COMMAND cmake -DCMAKE_INSTALL_PREFIX=${RIO_INSTALL_DIR}
+    INSTALL_COMMAND ""
     LOG_DOWNLOAD 1
     LOG_BUILD 1
     STEP_TARGETS build)
